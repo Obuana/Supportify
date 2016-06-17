a9f27a994fdd29592ff8e3d9425218f12c21efcd
DELANNOY <merlin.delannoy@capgemini.com>
2016-06-17 16:17:30 +0200

    Blocage de l'application en mode portrait et début de mise en place d'une interface pour la photo

--- Facedetection/app/src/main/java/euro/faucheisti/facedetection/FaceDetection.java
+++ Facedetection/app/src/main/java/euro/faucheisti/facedetection/FaceDetection.java
@@ -2,23 +2,18 @@ package euro.faucheisti.facedetection;
 
 import android.Manifest;
 import android.annotation.TargetApi;
-import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
+import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
-import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -32,20 +27,22 @@ public class FaceDetection extends AppCompatActivity {
 
     private Context context = this;
 
-    private Bitmap myBitmap;
-
     String mCurrentPhotoPath;
-    private Uri fileUri;
 
 
     private int OUVRIR_GALERIE = 1;
     private int PRENDRE_PHOTO = 2;
 
+    private myView mView;
 
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_face_detection);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
 
         // Demande la permission de lire des fichiers du stockage de l'appareil
         ActivityCompat.requestPermissions(this,
@@ -56,6 +53,8 @@ public class FaceDetection extends AppCompatActivity {
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                 1);
+
+        mView = (myView) findViewById(R.id.view);
 
     }
 
@@ -75,7 +74,6 @@ public class FaceDetection extends AppCompatActivity {
             File photoFile = null;
             try {
                 photoFile = createImageFile();
-                System.out.println(photoFile);
             } catch (IOException ex) {
 
             }
@@ -103,89 +101,17 @@ public class FaceDetection extends AppCompatActivity {
             // Données récupérée de l'intent
             Uri uri = data.getData();
 
-            try {
+            String path = uri.toString();
 
-                    // Création du bitmap à partir de l'uri
-                Bitmap tmpBitmap;
-                BitmapFactory.Options BitmapFactoryOptionsbfo = new BitmapFactory.Options();
-                BitmapFactoryOptionsbfo.inPreferredConfig = Bitmap.Config.RGB_565;
-                tmpBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                myBitmap = tmpBitmap.copy(Bitmap.Config.RGB_565, true);
-
-                // Code récupérer permettant de tourner la photo suivant l'orientation de l'appareil
-                ExifInterface exif = null;
-                try {
-                    exif = new ExifInterface(getPath(context,uri));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                int orientation = ExifInterface.ORIENTATION_NORMAL;
-
-                if (exif != null)
-                    orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-
-                switch (orientation) {
-                    case ExifInterface.ORIENTATION_ROTATE_90:
-                        myBitmap = rotateBitmap(myBitmap, 90);
-                        break;
-                    case ExifInterface.ORIENTATION_ROTATE_180:
-                        myBitmap = rotateBitmap(myBitmap, 180);
-                        break;
-
-                    case ExifInterface.ORIENTATION_ROTATE_270:
-                        myBitmap = rotateBitmap(myBitmap, 270);
-                        break;
-                }
+            // Crée la vue contenant la photo modifiée et lui passe le bitmap de la photo choisie
+            mView.setBitmapUriPath(path);
 
 
-                // Crée la vue contenant la photo modifiée et lui passe le bitmap de la photo choisie
-                myView mView = new myView(context, myBitmap);
-                setContentView(mView);
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         } else if (requestCode == PRENDRE_PHOTO ){
-            if (resultCode == RESULT_OK  ){
-
-                // Récupère la photo créée par l'appareil dans un bitamp pour la passée à la vue
-                BitmapFactory.Options BitmapFactoryOptionsbfo = new BitmapFactory.Options();
-                BitmapFactoryOptionsbfo.inPreferredConfig = Bitmap.Config.RGB_565;
-                BitmapFactoryOptionsbfo.inJustDecodeBounds = false;
-                System.out.println(mCurrentPhotoPath);
-                myBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, BitmapFactoryOptionsbfo);
-
-                // Code récupérer permettant de tourner la photo suivant l'orientation de l'appareil
-                ExifInterface exif = null;
-                try {
-                    exif = new ExifInterface(mCurrentPhotoPath);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                int orientation = ExifInterface.ORIENTATION_NORMAL;
-
-                if (exif != null)
-                    orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-
-                switch (orientation) {
-                    case ExifInterface.ORIENTATION_ROTATE_90:
-                        myBitmap = rotateBitmap(myBitmap, 90);
-                        break;
-                    case ExifInterface.ORIENTATION_ROTATE_180:
-                        myBitmap = rotateBitmap(myBitmap, 180);
-                        break;
-
-                    case ExifInterface.ORIENTATION_ROTATE_270:
-                        myBitmap = rotateBitmap(myBitmap, 270);
-                        break;
-                }
+            if (resultCode ==  RESULT_OK  ){
 
                 // Crée la vue contenant la photo modifiée et lui passe le bitmap de la photo prise
-                myView mView = new myView(context, myBitmap);
-                setContentView(mView);
+                mView.setBitmapPath(mCurrentPhotoPath);
 
             }else if (resultCode == RESULT_CANCELED) {
                 // User cancelled the image capture
@@ -198,132 +124,7 @@ public class FaceDetection extends AppCompatActivity {
     }
 
 
-    // Code récupérer permettant de tourner la photo suivant l'orientation de l'appareil
-    @TargetApi(Build.VERSION_CODES.KITKAT)
-    public static String getPath(final Context context, final Uri uri) {
 
-        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
-
-        // DocumentProvider
-        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-            // ExternalStorageProvider
-            if (isExternalStorageDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                if ("primary".equalsIgnoreCase(type)) {
-                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-                }
-
-                // TODO handle non-primary volumes
-            }
-            // DownloadsProvider
-            else if (isDownloadsDocument(uri)) {
-
-                final String id = DocumentsContract.getDocumentId(uri);
-                final Uri contentUri = ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-
-                return getDataColumn(context, contentUri, null, null);
-            }
-            // MediaProvider
-            else if (isMediaDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                Uri contentUri = null;
-                if ("image".equals(type)) {
-                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                } else if ("video".equals(type)) {
-                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                } else if ("audio".equals(type)) {
-                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                }
-
-                final String selection = "_id=?";
-                final String[] selectionArgs = new String[] {
-                        split[1]
-                };
-
-                return getDataColumn(context, contentUri, selection, selectionArgs);
-            }
-        }
-        // MediaStore (and general)
-        else if ("content".equalsIgnoreCase(uri.getScheme())) {
-            return getDataColumn(context, uri, null, null);
-        }
-        // File
-        else if ("file".equalsIgnoreCase(uri.getScheme())) {
-            return uri.getPath();
-        }
-
-        return null;
-    }
-
-    /**
-     * Get the value of the data column for this Uri. This is useful for
-     * MediaStore Uris, and other file-based ContentProviders.
-     *
-     * @param context The context.
-     * @param uri The Uri to query.
-     * @param selection (Optional) Filter used in the query.
-     * @param selectionArgs (Optional) Selection arguments used in the query.
-     * @return The value of the _data column, which is typically a file path.
-     */
-    public static String getDataColumn(Context context, Uri uri, String selection,
-                                       String[] selectionArgs) {
-
-        Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection = {
-                column
-        };
-
-        try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
-                    null);
-            if (cursor != null && cursor.moveToFirst()) {
-                final int column_index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(column_index);
-            }
-        } finally {
-            if (cursor != null)
-                cursor.close();
-        }
-        return null;
-    }
-
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is ExternalStorageProvider.
-     */
-    public static boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is DownloadsProvider.
-     */
-    public static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is MediaProvider.
-     */
-    public static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-    public static Bitmap rotateBitmap(Bitmap bitmap, int degrees) {
-        Matrix matrix = new Matrix();
-        matrix.postRotate(degrees);
-        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-    }
 
     // Code récupérer pour enregister une photo dans un fichier
     private File createImageFile() throws IOException {
--- Facedetection/app/src/main/java/euro/faucheisti/facedetection/myView.java
+++ Facedetection/app/src/main/java/euro/faucheisti/facedetection/myView.java
@@ -1,16 +1,33 @@
 package euro.faucheisti.facedetection;
 
 
+import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.media.ExifInterface;
 import android.media.FaceDetector;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import java.io.IOException;
 
 public class myView extends View {
 
@@ -21,53 +38,250 @@ public class myView extends View {
     private FaceDetector.Face[] myFace;
     private float myEyesDistance;
     int numberOfFaceDetected;
-    private Bundle data = new Bundle();
+
+    private String bitmapUriPath;
+    private String bitmapPath;
+
+    BitmapFactory.Options BitmapFactoryOptionsbfo;
 
     Bitmap myBitmap;
     Bitmap myBitmap2;
 
+    Context context;
 
+    public myView(Context context, AttributeSet attrs){
+        super(context, attrs);
 
-    public myView(Context context, Bitmap myBitmap){
-        super(context);
+        this.context = context;
 
-        BitmapFactory.Options BitmapFactoryOptionsbfo = new BitmapFactory.Options();
+        TypedArray attr = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.myView, 0, 0);
+
+        BitmapFactoryOptionsbfo = new BitmapFactory.Options();
         BitmapFactoryOptionsbfo.inPreferredConfig = Bitmap.Config.RGB_565;
-        this.myBitmap = myBitmap;
+        bitmapUriPath = attr.getString(R.styleable.myView_bitmapUriPath);
+        bitmapPath = attr.getString(R.styleable.myView_bitmapPath);
+
+
 
         myBitmap2 = BitmapFactory.decodeResource(getResources(),R.drawable.perruque,BitmapFactoryOptionsbfo);
 
     }
 
-
-
-
-
     @Override
     protected void onDraw(Canvas canvas) {
 
-        myBitmap = Bitmap.createScaledBitmap(myBitmap, this.getWidth(), this.getHeight(),true);
-        imageWidth = myBitmap.getWidth();
-        imageHeight = myBitmap.getHeight();
-        myFace = new FaceDetector.Face[numberOfFace];
-        myFaceDetect = new FaceDetector(imageWidth, imageHeight, numberOfFace);
-        numberOfFaceDetected = myFaceDetect.findFaces(myBitmap, myFace);
-        canvas.drawBitmap(myBitmap, 0, 0, null);
-        Paint myPaint = new Paint();
-        myPaint.setColor(Color.GREEN);
-        myPaint.setStyle(Paint.Style.STROKE);
-        myPaint.setStrokeWidth(3);
+        ExifInterface exif = null;
+        try {
+            if(!bitmapUriPath.equals("")) {
+                myBitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), Uri.parse(bitmapUriPath));
+                myBitmap = myBitmap.copy(Bitmap.Config.RGB_565, true);
+                exif = new ExifInterface(getPath(context, Uri.parse(bitmapUriPath)));
+                bitmapUriPath = "";
 
-        if(numberOfFaceDetected!=0) {
-            FaceDetector.Face face = myFace[0];
-            PointF myMidPoint = new PointF();
-            face.getMidPoint(myMidPoint);
-            myEyesDistance = face.eyesDistance();
-            myBitmap2 = Bitmap.createScaledBitmap(myBitmap2,(int) myEyesDistance * 5, (int)( myEyesDistance * 5),true);
+            }else if(!bitmapPath.equals("")){
+                myBitmap = BitmapFactory.decodeFile(bitmapPath, BitmapFactoryOptionsbfo);
+                exif = new ExifInterface(bitmapPath);
+                bitmapPath = "";
 
-            canvas.drawBitmap(myBitmap2,(float) (myMidPoint.x - myEyesDistance * 2.5),(float) (myMidPoint.y - myEyesDistance * 3.4), null);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
+        if (myBitmap != null) {
+            // Code récupérer permettant de tourner la photo suivant l'orientation de l'appareil
+            int orientation = ExifInterface.ORIENTATION_NORMAL;
+
+            if (exif != null)
+                orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    myBitmap = rotateBitmap(myBitmap, 90);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    myBitmap = rotateBitmap(myBitmap, 180);
+                    break;
+
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    myBitmap = rotateBitmap(myBitmap, 270);
+                    break;
+            }
+
+        }
+
+        if (myBitmap != null) {
+            myBitmap = Bitmap.createScaledBitmap(myBitmap, this.getWidth(), this.getHeight(), true);
+            imageWidth = myBitmap.getWidth();
+            imageHeight = myBitmap.getHeight();
+            myFace = new FaceDetector.Face[numberOfFace];
+            myFaceDetect = new FaceDetector(imageWidth, imageHeight, numberOfFace);
+            numberOfFaceDetected = myFaceDetect.findFaces(myBitmap, myFace);
+            canvas.drawBitmap(myBitmap, 0, 0, null);
+            Paint myPaint = new Paint();
+            myPaint.setColor(Color.GREEN);
+            myPaint.setStyle(Paint.Style.STROKE);
+            myPaint.setStrokeWidth(3);
+
+            if (numberOfFaceDetected != 0) {
+                FaceDetector.Face face = myFace[0];
+                PointF myMidPoint = new PointF();
+                face.getMidPoint(myMidPoint);
+                myEyesDistance = face.eyesDistance();
+                myBitmap2 = Bitmap.createScaledBitmap(myBitmap2, (int) myEyesDistance * 5, (int) (myEyesDistance * 5), true);
+
+                canvas.drawBitmap(myBitmap2, (float) (myMidPoint.x - myEyesDistance * 2.5), (float) (myMidPoint.y - myEyesDistance * 3.4), null);
+            }
+
+        }
+    }
+
+    public String getBitmapUriPath() {
+        return bitmapUriPath;
+    }
+
+    public String getBitmapPath() {
+        return bitmapPath;
+    }
+
+    public void setBitmapUriPath(String bitmapUriPath) {
+        this.bitmapUriPath = bitmapUriPath;
+        invalidate();
+        requestLayout();
+    }
+
+    public void setBitmapPath(String bitmapPath) {
+        this.bitmapPath = bitmapPath;
+        invalidate();
+        requestLayout();
+    }
+
+    // Code récupérer permettant de tourner la photo suivant l'orientation de l'appareil
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context The context.
+     * @param uri The Uri to query.
+     * @param selection (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+    public static Bitmap rotateBitmap(Bitmap bitmap, int degrees) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degrees);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
     }
 
 
